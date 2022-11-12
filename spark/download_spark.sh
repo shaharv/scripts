29fd2a410fbac4b836e19d9452955b78036ab0e2
Shahar Valiano <shahar.valiano@gmail.com>
2022-11-12 10:46:45 +0200

    Add Spark 3.3.1 download script

---
+++ spark/download_spark.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DELIM="================================================================================"
+SPARK_VER=3.3.1
+SPARK_DOWNLOAD_URL=https://dlcdn.apache.org/spark/spark-$SPARK_VER
+SPARK_RELEASE=spark-$SPARK_VER-bin-hadoop3
+SPARK_ZIP=$SPARK_RELEASE.tgz
+SPARK_ASC_FILE=$SPARK_ZIP.asc
+
+# Download spark into a new folder
+echo $DELIM
+echo "Downloading Spark $SPARK_VER..."
+echo $DELIM
+TEMP_DIR=$(mktemp -d -t spark-XXXXXXXX)
+cd $TEMP_DIR
+wget $SPARK_DOWNLOAD_URL/$SPARK_ASC_FILE
+wget $SPARK_DOWNLOAD_URL/$SPARK_ZIP
+
+# Verify the downloaded zip based on instructions in:
+# - https://www.apache.org/dyn/closer.lua/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
+# - https://www.apache.org/info/verification.html
+echo $DELIM
+echo "Verifying downloaded zip..."
+echo $DELIM
+wget https://downloads.apache.org/spark/KEYS
+gpg --import KEYS
+gpg --verify $SPARK_ASC_FILE $SPARK_ZIP
+
+# Extract Spark
+echo $DELIM
+echo "Extracting Spark..."
+echo $DELIM
+tar xvf $SPARK_ZIP
+
+# Finish
+echo $DELIM
+echo "Spark $SPARK_VER was extracted to $TEMP_DIR/$SPARK_RELEASE."
+echo $DELIM
