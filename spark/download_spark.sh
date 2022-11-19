f9724281eb11f51a54eabeeb75e205e24e841e0b
Shahar Valiano <shahar.valiano@gmail.com>
2022-11-19 16:10:22 +0200

    Spark: verify sha512 checksum

--- spark/download_spark.sh
+++ spark/download_spark.sh
@@ -8,6 +8,7 @@ SPARK_DOWNLOAD_URL=https://dlcdn.apache.org/spark/spark-$SPARK_VER
 SPARK_RELEASE=spark-$SPARK_VER-bin-hadoop3
 SPARK_ZIP=$SPARK_RELEASE.tgz
 SPARK_ASC_FILE=$SPARK_ZIP.asc
+SPARK_SHA_FILE=$SPARK_ZIP.sha512
 
 # Download spark into a new folder
 echo $DELIM
@@ -16,9 +17,10 @@ echo $DELIM
 TEMP_DIR=$(mktemp -d -t spark-XXXXXXXX)
 cd $TEMP_DIR
 wget $SPARK_DOWNLOAD_URL/$SPARK_ASC_FILE
+wget $SPARK_DOWNLOAD_URL/$SPARK_SHA_FILE
 wget $SPARK_DOWNLOAD_URL/$SPARK_ZIP
 
-# Verify the downloaded zip based on instructions in:
+# Verify the downloaded zip signature and hash based on instructions in:
 # - https://www.apache.org/dyn/closer.lua/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
 # - https://www.apache.org/info/verification.html
 echo $DELIM
@@ -27,6 +29,8 @@ echo $DELIM
 wget https://downloads.apache.org/spark/KEYS
 gpg --import KEYS
 gpg --verify $SPARK_ASC_FILE $SPARK_ZIP
+sha512sum -c $SPARK_SHA_FILE
+echo "sha512 checksum verified."
 
 # Extract Spark
 echo $DELIM
