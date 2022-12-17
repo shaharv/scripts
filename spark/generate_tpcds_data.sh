b047ec1319ecae607a65322f89ee6faef1be0340
Shahar Valiano <shahar.valiano@gmail.com>
2022-12-17 18:06:12 +0200

    Spark: download TPC-DS queries, fix bash -u mode

--- spark/generate_tpcds_data.sh
+++ spark/generate_tpcds_data.sh
@@ -3,27 +3,37 @@
 set -euo pipefail
 
 DELIM="================================================================================"
-SPARK_TPCDS_GEN_BRANCH=master
-SPARK_TPCDS_ZIP_NAME=spark-tpcds-datagen-$SPARK_TPCDS_GEN_BRANCH
+SPARK_TPCDS_ZIP_NAME=spark-tpcds-datagen-master
+TPCDS_QUERIES_ZIP_NAME=tpcds-master
 TPCDS_GEN_DIR=$(mktemp -d -t tpcds-gen-XXXXXXXX)
-OUTPUT_DATA_DIR=$(mktemp -d -t tpcds-data-XXXXXXXX)
+TPCDS_TEMP_DIR=$(mktemp -d -t tpcds-data-XXXXXXXX)
+TPCDS_DATA_DIR=$TPCDS_TEMP_DIR/tpcds-1gb
 SCALE_FACTOR=1
 
-if [ -z $SPARK_HOME ]; then
+if [ -z ${SPARK_HOME:-} ]; then
     echo "Please set SPARK_HOME."
     exit 1
 fi
 
-if [ -z $JAVA_HOME ]; then
+if [ -z ${JAVA_HOME:-} ]; then
     echo "Please set JAVA_HOME."
     exit 1
 fi
 
 echo $DELIM
+echo "Downloading TPC-DS queries..."
+echo $DELIM
+cd $TPCDS_TEMP_DIR
+curl https://codeload.github.com/Agirish/tpcds/zip/refs/heads/master --output $TPCDS_QUERIES_ZIP_NAME.zip
+unzip $TPCDS_QUERIES_ZIP_NAME.zip
+rm $TPCDS_QUERIES_ZIP_NAME.zip
+mv tpcds-master tpcds-queries
+
+echo $DELIM
 echo "Downloading TPC-DS generator..."
 echo $DELIM
 cd $TPCDS_GEN_DIR
-curl https://codeload.github.com/maropu/spark-tpcds-datagen/zip/refs/heads/$SPARK_TPCDS_GEN_BRANCH --output $SPARK_TPCDS_ZIP_NAME.zip
+curl https://codeload.github.com/maropu/spark-tpcds-datagen/zip/refs/heads/master --output $SPARK_TPCDS_ZIP_NAME.zip
 
 echo $DELIM
 echo "Extracting TPC-DS generator..."
@@ -38,9 +48,15 @@ echo $DELIM
 echo "Generating TPC-DS data for SF=$SCALE_FACTOR..."
 echo $DELIM
 cd $TPCDS_GEN_DIR/$SPARK_TPCDS_ZIP_NAME
-bin/dsdgen --output-location $OUTPUT_DATA_DIR --scale-factor $SCALE_FACTOR --format parquet
+bin/dsdgen --output-location $TPCDS_DATA_DIR --scale-factor $SCALE_FACTOR --format parquet
+
+echo "Removing temporary files..."
+rm -r $TPCDS_GEN_DIR
 
 echo $DELIM
-echo "TPC-DS data successfully generated under $OUTPUT_DATA_DIR."
-echo "For further stages set: export TPCDS_DATA_DIR=$OUTPUT_DATA_DIR"
+echo "TPC-DS data successfully generated under $TPCDS_TEMP_DIR."
+echo "For further stages set:"
+echo
+echo "export TPCDS_DATA_DIR=$TPCDS_DATA_DIR"
+echo "export TPCDS_QUERIES_DIR=$TPCDS_TEMP_DIR/tpcds-queries"
 echo $DELIM
