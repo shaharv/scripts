5c4467c084e128a026ba16b7fe6fb9b348ab7838
Shahar Valiano <shahar.valiano@gmail.com>
2023-09-10 10:55:03 +0300

    llvm_install.sh improvements

--- bash/llvm_install.sh
+++ bash/llvm_install.sh
@@ -2,33 +2,55 @@
 
 set -euo pipefail
 
+function usage {
+    echo "Usage: $0 --version <N> [options]"
+    echo
+    echo "Where N is the LLVM major version. For example: $0 --version=16"
+    echo
+    echo "Options:"
+    echo "--help       Display this usage"
+}
+
 if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 <llvm major version>"
-    exit 1
+    usage
+    exit 0
 fi
 
-LLVM_VERSION=$1
+LLVM_VERSION=0
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --help) usage; exit 0;;
+        --version) shift; LLVM_VERSION=$1; shift;;
+        *) echo "Error: unknown argument: $1"; usage; exit 1;;
+    esac
+done
+
 NUMBER_REGEX='^[0-9]+$'
 if ! [[ $LLVM_VERSION =~ $NUMBER_REGEX ]]; then
     echo "Error: invalid version specified: $LLVM_VERSION."
+    usage
     exit 1
 fi
-
-PRIORITY=$(( $LLVM_VERSION * 100 ))
-DISTRO=$(lsb_release -c -s)
 
 if [ `id -u` != 0 ] ; then
-    echo "Must be run as root!"
+    echo "Must be run as sudo!"
     exit 1
 fi
 
+echo "Installing LLVM version $LLVM_VERSION."
+
+# Install prerequisite packages
+apt-get update
+apt-get install -y --no-install-recommends software-properties-common wget gnupg
+
 # Set up LLVM apt repository
-apt install -y --no-install-recommends software-properties-common wget gnupg
+DISTRO=$(lsb_release -c -s)
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-add-apt-repository "deb http://apt.llvm.org/$DISTRO/ llvm-toolchain-$DISTRO-$LLVM_VERSION main" && apt update
+add-apt-repository "deb http://apt.llvm.org/$DISTRO/ llvm-toolchain-$DISTRO-$LLVM_VERSION main"
+apt-get update
 
 # Install LLVM tools
-apt install -y \
+apt-get install -y \
     clang-$LLVM_VERSION \
     clang-format-$LLVM_VERSION \
     clang-tidy-$LLVM_VERSION \
@@ -37,7 +59,7 @@ apt install -y \
     libc++-$LLVM_VERSION-dev \
     libc++abi-$LLVM_VERSION-dev
 
-# Update alternatives
+PRIORITY=$(( $LLVM_VERSION * 100 ))
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION $PRIORITY
 update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-$LLVM_VERSION $PRIORITY
 update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-$LLVM_VERSION $PRIORITY
