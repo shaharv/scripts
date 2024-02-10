154c907e79399fd8755352096fa02c9d41c95c81
Shahar Valiano <shahar.valiano@gmail.com>
2024-02-10 17:02:51 +0200

    Add jenkins_utils.groovy

---
+++ jenkins/jenkins_utils.groovy
@@ -0,0 +1,4 @@
+def isDefinedNotEmpty(arg) {
+    // validating that arg is not null object and the value of arg is not empty or "null"
+    return (arg == 0) || (arg && (arg != "null") && (arg != "") && ("${arg}" != "null"))
+}
