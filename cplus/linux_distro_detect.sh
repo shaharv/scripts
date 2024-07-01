8fadf36335d4d3c23d98e14c584d3f56e99829b0
Shahar Valiano <shahar.valiano@gmail.com>
2024-07-01 22:34:32 +0300

    Add linux_distro_detect.sh

---
+++ cplus/linux_distro_detect.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function getField {
+   local field=$1
+   echo $(grep "\b$field\=" /etc/os-release | sed s/^$field\=// | tr -d '"')
+}
+
+# Extract Linux OS properties from /etc/os-release.
+# This method is used instead of lsb_release, since lsb_release is not available for
+# all distributions.
+export LINUX_DISTRO=$(getField "ID")
+export LINUX_RELEASE_PRETTY_NAME=$(getField "PRETTY_NAME")
+export LINUX_RELEASE_NAME=$(getField "VERSION_CODENAME")
+export LINUX_RELEASE_NUM=$(getField "VERSION_ID")
+export LINUX_RELEASE_NUM_MAJOR=${LINUX_RELEASE_NUM%.*}
+
+# Detect the package manager
+if command -v apt > /dev/null; then
+    INSTALL_CMD="apt-get install -y --no-install-recommends"
+    UPDATE_CMD="apt-get update -y"
+    REMOVE_CMD="apt-get remove -y"
+    AUTOREMOVE_CMD="apt-get autoremove -y"
+elif command -v yum >/dev/null; then
+    if [[ $LINUX_RELEASE_NUM_MAJOR -ge 8 ]]; then
+        INSTALL_CMD="yum install -y --allowerasing"
+    else
+        INSTALL_CMD="yum install -y"
+    fi
+    UPDATE_CMD="yum update -y"
+    REMOVE_CMD="yum remove -y"
+    AUTOREMOVE_CMD="yum autoremove -y"
+else
+    echo "Unsupported package manager (not apt or yum)."
+    exit 1
+fi
+
+if [ "${1:-}" = "--verbose" ]; then
+    echo "Linux OS details:"
+    echo "LINUX_DISTRO = $LINUX_DISTRO"
+    echo "LINUX_RELEASE_PRETTY_NAME = $LINUX_RELEASE_PRETTY_NAME"
+    echo "LINUX_RELEASE_NAME = $LINUX_RELEASE_NAME"
+    echo "LINUX_RELEASE_NUM = $LINUX_RELEASE_NUM"
+    echo "LINUX_RELEASE_MAJOR = $LINUX_RELEASE_NUM_MAJOR"
+    echo
+    echo "Package manager commands:"
+    echo "INSTALL_CMD = $INSTALL_CMD"
+    echo "UPDATE_CMD = $UPDATE_CMD"
+    echo "REMOVE_CMD = $REMOVE_CMD"
+    echo "AUTOREMOVE_CMD = $AUTOREMOVE_CMD"
+fi
