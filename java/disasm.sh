a8a781dc34576700ba6cf3cb64af18b95eb8f589
Shahar Valiano <shahar.valiano@gmail.com>
2024-08-02 17:32:08 +0300

    Remove old java disasm scripts

--- java/disasm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -exuo pipefail
-
-$JAVA_HOME/bin/javap -p -l -v -s -c $1
--- java/disasm_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -exuo pipefail
-
-CLASS_FILES=`find -maxdepth 1 -name "*.class"`
-
-for CLASS_FILE in ${CLASS_FILES[*]}; do
-  source disasm.sh $CLASS_FILE > $CLASS_FILE.txt
-done
