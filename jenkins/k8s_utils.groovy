78462802eea7a159eea033498ef228a47f639f0e
Shahar Valiano <shahar.valiano@gmail.com>
2024-09-04 22:11:29 +0300

    k8s_utils.groovy improvements:
    - Rename getPodName to getRunningPodName
    - Implement getNewestPodName
    - Implement forceKillAllDockerContainers
    - Add default parameters to waitForPods
    - Annotate and reorder fullK8sCleanup

--- jenkins/k8s_utils.groovy
+++ jenkins/k8s_utils.groovy
@@ -1,10 +1,17 @@
-// Given a pod name string, returns the name of a running pod instance.
+// Given a pod name string, returns the name of a running pod.
 // The output of "kubectl get pods" is filtered to get running pods that match the name.
 // Only one line of output is used - it is assumed no more than one pod instance is running.
 // Extra "grep Running" is needed, as kubectl will also return pods with "Terminating" state,
 // which are still running technically.
-String getPodName(String podNameString) {
+String getRunningPodName(String podNameString) {
     def podName = sh(script: "kubectl get pods --no-headers --field-selector=status.phase=Running --selector=app.kubernetes.io/name=$podNameString -o wide | grep Running | cut -d' ' -f1 | head -1", returnStdout: true).trim()
+    return podName
+}
+
+// Given a pod name string, returns the name of a most recent concrete pod.
+// The returned pod might not be in a Running state; for running pods, use getRunningPodName.
+String getNewestPodName(String podNameString) {
+    def podName = sh(script: "kubectl get pods --no-headers --selector=app.kubernetes.io/name=$podNameString --all-namespaces -o custom-columns=NAME:.metadata.name,AGE:.metadata.creationTimestamp | sort -k2 -r | awk '{print \$1}' | head -1", returnStdout: true).trim()
     return podName
 }
 
@@ -33,14 +40,12 @@ void runInPod(String podName, String containerName, String runCommand) {
     sh "rm ${scriptPath}"
 }
 
-boolean waitForPods(List<String> podNames) {
-    def repeats = 15
-    def sleepSeconds = 10
+boolean waitForPods(List<String> podNames, sleepSeconds = 10, repeats = 15) {
     for (int i = 1; i <= repeats; i++) {
         def podsStarted = 0
         for(podName in podNames) {
             echo "Waiting for " + podName + " (" + i + ") ..."
-            def podInstance = k8sUtils.getPodName(podName)
+            def podInstance = k8sUtils.getRunningPodName(podName)
             if (podInstance != "") {
                 echo "Pod started: " + podInstance
                 podsStarted++
@@ -54,16 +59,35 @@ boolean waitForPods(List<String> podNames) {
     return false
 }
 
-// Perform a complete cleanup of K8s
+// Perform a complete cleanup of the K8s runtime.
+// Docker images are not impacted (those reside in the local docker image registry).
 void fullK8sCleanup() {
-    sh "kubectl delete deployments  --all --all-namespaces         || true"
-    sh "kubectl delete services     --all --all-namespaces         || true"
+    // Forcefully stop and remove running pods, including kube-system pods.
+    // System pods, such as Calico pods, will be restored by their daemonSet.
     sh "kubectl delete pods         --all --all-namespaces --force || true"
-    sh "kubectl delete daemonsets   --all --all-namespaces         || true"
+
+    // Remove pod lifecycle management entities.
+    // Do not remove deployments and daemonsets from the kube-system namespace,
+    // for not deleting entities of Calico (network plugin).
+    // Note that in case Calico is removed, it could be restored using the following command:
+    // "kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml".
+    sh "kubectl delete deployments  --all                          || true"
     sh "kubectl delete statefulsets --all --all-namespaces         || true"
+    sh "kubectl delete daemonsets   --all                          || true"
     sh "kubectl delete jobs         --all --all-namespaces         || true"
+
+    // Remove services, which manage pods' networking
+    sh "kubectl delete services     --all --all-namespaces         || true"
+
+    // Remove pod storage related entities
     sh "kubectl delete pvc          --all --all-namespaces         || true"
     sh "kubectl delete pv           --all                          || true"
+}
+
+// Force kill all docker containers using their pid.
+// This allows killing containers which could be stopped even with --force.
+void forceKillAllDockerContainers() {
+    sh "docker ps -q | xargs -I {} docker inspect --format '{{ .State.Pid }}' {} | xargs sudo kill -9 || true"
 }
 
 return this
