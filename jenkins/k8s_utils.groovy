20629d277e19f5db868a9a81411157b414a7603c
Shahar Valiano <shahar.valiano@gmail.com>
2024-09-08 19:36:39 +0300

    k8s_utils.groovy improvements:
    - Add namespace parameter to getRunningPodName and getNewestPodName
    - Implement printAllPodsStatus
    - Improved teardown procedure in fullK8sCleanup

--- jenkins/k8s_utils.groovy
+++ jenkins/k8s_utils.groovy
@@ -1,18 +1,26 @@
-// Given a pod name string, returns the name of a running pod.
+// Given a pod name string and optional namespace, returns the name of a running pod.
 // The output of "kubectl get pods" is filtered to get running pods that match the name.
 // Only one line of output is used - it is assumed no more than one pod instance is running.
 // Extra "grep Running" is needed, as kubectl will also return pods with "Terminating" state,
 // which are still running technically.
-String getRunningPodName(String podNameString) {
-    def podName = sh(script: "kubectl get pods --no-headers --field-selector=status.phase=Running --selector=app.kubernetes.io/name=$podNameString -o wide | grep Running | cut -d' ' -f1 | head -1", returnStdout: true).trim()
+String getRunningPodName(String podNameString, String podNameSpace = "default") {
+    def podName = sh(script: "kubectl get pods --no-headers -n $podNameSpace --field-selector=status.phase=Running --selector=app.kubernetes.io/name=$podNameString -o wide | grep Running | cut -d' ' -f1 | head -1", returnStdout: true).trim()
     return podName
 }
 
 // Given a pod name string, returns the name of a most recent concrete pod.
 // The returned pod might not be in a Running state; for running pods, use getRunningPodName.
-String getNewestPodName(String podNameString) {
-    def podName = sh(script: "kubectl get pods --no-headers --selector=app.kubernetes.io/name=$podNameString --all-namespaces -o custom-columns=NAME:.metadata.name,AGE:.metadata.creationTimestamp | sort -k2 -r | awk '{print \$1}' | head -1", returnStdout: true).trim()
+String getNewestPodName(String podNameString, String podNameSpace = "default") {
+    def podName = sh(script: "kubectl get pods --no-headers -n $podNameSpace --selector=app.kubernetes.io/name=$podNameString -o custom-columns=NAME:.metadata.name,AGE:.metadata.creationTimestamp | sort -k2 -r | awk '{print \$1}' | head -1", returnStdout: true).trim()
     return podName
+}
+
+// Print an abbreviated or detailed status of all K8s pods including system ones.
+void printAllPodsStatus(boolean detailed = false) {
+    sh "kubectl get pods --all-namespaces"
+    if (detailed) {
+        sh "kubectl describe pod --all-namespaces"
+    }
 }
 
 // Run the specified command inside the specified pod instance.
@@ -59,29 +67,25 @@ boolean waitForPods(List<String> podNames, sleepSeconds = 10, repeats = 15) {
     return false
 }
 
-// Perform a complete cleanup of the K8s runtime.
-// Docker images are not impacted (those reside in the local docker image registry).
+// Perform a cleanup of the K8s runtime in the default workspace.
+// The K8s system entities (under the kube-system and ingress namespaces) are not impacted.
+// Docker images are also not impacted - those reside in the local docker image registry.
 void fullK8sCleanup() {
-    // Forcefully stop and remove running pods, including kube-system pods.
-    // System pods, such as Calico pods, will be restored by their daemonSet.
-    sh "kubectl delete pods         --all --all-namespaces --force || true"
+    // Forcefully stop and remove running pods in the default workspace.
+    sh "kubectl delete pods         --all --force || true"
 
-    // Remove pod lifecycle management entities.
-    // Do not remove deployments and daemonsets from the kube-system namespace,
-    // for not deleting entities of Calico (network plugin).
-    // Note that in case Calico is removed, it could be restored using the following command:
-    // "kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml".
-    sh "kubectl delete deployments  --all                          || true"
-    sh "kubectl delete statefulsets --all --all-namespaces         || true"
-    sh "kubectl delete daemonsets   --all                          || true"
-    sh "kubectl delete jobs         --all --all-namespaces         || true"
+    // Remove pod lifecycle management entities
+    sh "kubectl delete deployments  --all || true"
+    sh "kubectl delete statefulsets --all || true"
+    sh "kubectl delete daemonsets   --all || true"
+    sh "kubectl delete jobs         --all || true"
 
     // Remove services, which manage pods' networking
-    sh "kubectl delete services     --all --all-namespaces         || true"
+    sh "kubectl delete services     --all || true"
 
     // Remove pod storage related entities
-    sh "kubectl delete pvc          --all --all-namespaces         || true"
-    sh "kubectl delete pv           --all                          || true"
+    sh "kubectl delete pvc          --all || true"
+    sh "kubectl delete pv           --all || true"
 }
 
 // Force kill all docker containers using their pid.
