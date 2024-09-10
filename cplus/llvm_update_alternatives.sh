07ffc42f306a0c4f9d8646c31a5a28c4653d65b3
Shahar Valiano <shahar.valiano@gmail.com>
2024-09-10 22:07:29 +0300

    Run update-alternatives with --force

--- cplus/llvm_update_alternatives.sh
+++ cplus/llvm_update_alternatives.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 function usage {
     echo "Usage: $0 --llvm-version <N>"
     echo
-    echo "Where N is the LLVM major version. For example: $0 --llvm-version 18"
+    echo "Where N is the LLVM major version. For example: $0 --llvm-version 19"
     echo
     echo "Options:"
     echo "--llvm-version <N> Set the LLVM version to be made default"
@@ -42,18 +42,18 @@ fi
 
 PRIORITY=$(( $LLVM_VERSION * 100 ))
 
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION $PRIORITY
-update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-$LLVM_VERSION $PRIORITY
-update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-$LLVM_VERSION $PRIORITY
-update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-$LLVM_VERSION $PRIORITY
-update-alternatives --install /usr/bin/lld lld /usr/bin/lld-$LLVM_VERSION $PRIORITY
-update-alternatives --install /usr/bin/ld.lld ld.lld /usr/bin/ld.lld-$LLVM_VERSION $PRIORITY
+update-alternatives --force --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION $PRIORITY
+update-alternatives --force --install /usr/bin/clang-format clang-format /usr/bin/clang-format-$LLVM_VERSION $PRIORITY
+update-alternatives --force --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-$LLVM_VERSION $PRIORITY
+update-alternatives --force --install /usr/bin/lldb lldb /usr/bin/lldb-$LLVM_VERSION $PRIORITY
+update-alternatives --force --install /usr/bin/lld lld /usr/bin/lld-$LLVM_VERSION $PRIORITY
+update-alternatives --force --install /usr/bin/ld.lld ld.lld /usr/bin/ld.lld-$LLVM_VERSION $PRIORITY
 
 # llvm-symbolizer is required for debug information in address sanitizer stack traces.
 # It should be set in the environment of the sanitized program by setting:
 # ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
 # See: https://stackoverflow.com/a/36757623/7256341
-update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-$LLVM_VERSION $PRIORITY
+update-alternatives --force --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-$LLVM_VERSION $PRIORITY
 
 # Install symlink to run-clang-tidy
 ln -sf $(readlink -f /usr/bin/run-clang-tidy-$LLVM_VERSION) /usr/bin/run-clang-tidy
