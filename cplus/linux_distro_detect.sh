2fb77647ceb0e8a1de52b847e82db18f2bb9bfac
Shahar Valiano <shahar.valiano@gmail.com>
2024-12-08 07:05:20 +0200

    linux_distro_detect.sh: add package properties; update apt install command

--- cplus/linux_distro_detect.sh
+++ cplus/linux_distro_detect.sh
@@ -18,10 +18,13 @@ export LINUX_RELEASE_NUM_MAJOR=${LINUX_RELEASE_NUM%.*}
 
 # Detect the package manager
 if command -v apt > /dev/null; then
-    INSTALL_CMD="apt-get install -y --no-install-recommends"
+    INSTALL_CMD="apt-get install -y --no-install-recommends --allow-downgrades"
     UPDATE_CMD="apt-get update -y"
     REMOVE_CMD="apt-get remove -y"
     AUTOREMOVE_CMD="apt-get autoremove -y"
+    PACKAGE_EXT="deb"
+    PACKAGE_SUFFIX="_amd64"
+    PACKAGE_SEP="_"
 elif command -v yum >/dev/null; then
     if [[ $LINUX_RELEASE_NUM_MAJOR -ge 8 ]]; then
         INSTALL_CMD="yum install -y --allowerasing"
@@ -31,6 +34,9 @@ elif command -v yum >/dev/null; then
     UPDATE_CMD="yum update -y"
     REMOVE_CMD="yum remove -y"
     AUTOREMOVE_CMD="yum autoremove -y"
+    PACKAGE_EXT="rpm"
+    PACKAGE_SUFFIX="-1.el8.x86_64"
+    PACKAGE_SEP="-"
 else
     echo "Unsupported package manager (not apt or yum)."
     exit 1
@@ -49,4 +55,9 @@ if [ "${1:-}" = "--verbose" ]; then
     echo "UPDATE_CMD = $UPDATE_CMD"
     echo "REMOVE_CMD = $REMOVE_CMD"
     echo "AUTOREMOVE_CMD = $AUTOREMOVE_CMD"
+    echo
+    echo "Package properties:"
+    echo "PACKAGE_EXT = $PACKAGE_EXT"
+    echo "PACKAGE_SUFFIX = $PACKAGE_SUFFIX"
+    echo "PACKAGE_SEP = $PACKAGE_SEP"
 fi
