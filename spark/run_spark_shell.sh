df5719f210bd9a5942f5619a166979d007179bff
Shahar Valiano <shahar.v@neuroblade.ai>
2025-03-09 14:59:41 +0200

    run_spark_shell.sh: support overriding memory and cores settings (2)

--- spark/run_spark_shell.sh
+++ spark/run_spark_shell.sh
@@ -16,7 +16,7 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}
 SPARK_DRIVER_CORES=${SPARK_DRIVER_CORES:-2}
 SPARK_EXECUTOR_CORES=${SPARK_EXECUTOR_CORES:-2}
 let SPARK_TOTAL_EXECUTOR_CORES=($SPARK_WORKER_CORES-$SPARK_DRIVER_CORES)
-SPARK_EXECUTOR_INSTANCES=$(bc <<< $SPARK_TOTAL_EXECUTOR_CORES/$SPARK_EXECUTOR_CORES)
+SPARK_EXECUTOR_INSTANCES=${SPARK_EXECUTOR_INSTANCES:-$(bc <<< $SPARK_TOTAL_EXECUTOR_CORES/$SPARK_EXECUTOR_CORES)}
 
 # Set spark.max.cores identical to SPARK_WORKER_CORES, meaning that a single
 # application will be served with all cores.
@@ -24,25 +24,22 @@ SPARK_CORES_MAX=$SPARK_WORKER_CORES
 
 # Set the same amount of memory per executor core.
 SPARK_DRIVER_MEMORY_GB=${SPARK_DRIVER_MEMORY_GB:-}
-SPARK_DRIVER_MEMORY=${SPARK_DRIVER_MEMORY_GB}"G"
 SPARK_WORKER_MEMORY_GB=${SPARK_WORKER_MEMORY//G}
 SPARK_EXECUTOR_MEMORY_GB=${SPARK_EXECUTOR_MEMORY_GB:-$(echo "scale=0; x=($SPARK_WORKER_MEMORY_GB-$SPARK_DRIVER_MEMORY_GB)/$SPARK_EXECUTOR_INSTANCES; if(x<1 && x>0) print 0; x" | bc)}
-SPARK_EXECUTOR_MEMORY=${SPARK_EXECUTOR_MEMORY_GB}"G"
 SPARK_EXECUTOR_MEMORY_OVERHEAD_GB=${SPARK_EXECUTOR_MEMORY_OVERHEAD_GB:-1}
-SPARK_EXECUTOR_MEMORY_OVERHEAD=${SPARK_EXECUTOR_MEMORY_OVERHEAD_GB}"G"
 
 # Set the location of Spark metastore_db and spark-warehouse folders
 SPARK_DIRS=${SPARK_DIRS:-/tmp}
 
 echo "Starting spark-shell..."
 echo
-echo "Spark worker memory:   $SPARK_WORKER_MEMORY"
-echo "Spark worker cores:    $SPARK_WORKER_CORES"
-echo "Spark driver memory:   $SPARK_DRIVER_MEMORY"
-echo "Spark driver cores:    $SPARK_DRIVER_CORES"
-echo "Spark executors:       $SPARK_EXECUTOR_INSTANCES"
-echo "Spark executor memory: $SPARK_EXECUTOR_MEMORY"
-echo "Spark executor cores:  $SPARK_EXECUTOR_CORES"
+echo "Spark worker memory (GB):   $SPARK_WORKER_MEMORY_GB"
+echo "Spark worker cores:         $SPARK_WORKER_CORES"
+echo "Spark driver memory (GB):   $SPARK_DRIVER_MEMORY_GB"
+echo "Spark driver cores:         $SPARK_DRIVER_CORES"
+echo "Spark executors:            $SPARK_EXECUTOR_INSTANCES"
+echo "Spark executor memory (GB): $SPARK_EXECUTOR_MEMORY_GB"
+echo "Spark executor cores:       $SPARK_EXECUTOR_CORES"
 echo
 
 GLUTEN_JAR=${GLUTEN_JAR:-gluten-velox-bundle-spark3.5_2.12-centos_7_x86_64-1.3.0.jar}
@@ -51,7 +48,7 @@ GLUTEN_OPTIONS=" \
     --conf spark.plugins=org.apache.gluten.GlutenPlugin \
     --conf spark.gluten.sql.debug=true \
     --conf spark.memory.offHeap.enabled=true \
-    --conf spark.memory.offHeap.size=${SPARK_EXECUTOR_MEMORY} \
+    --conf spark.memory.offHeap.size="${SPARK_EXECUTOR_MEMORY_GB}G" \
     --conf spark.shuffle.manager=org.apache.spark.shuffle.sort.ColumnarShuffleManager \
     --jars $SPARK_HOME/jars/$GLUTEN_JAR"
 
@@ -70,7 +67,7 @@ ${SPARK_HOME}/bin/spark-shell \
     --conf spark.driver.extraJavaOptions="-Dio.netty.tryReflectionSetAccessible=true" \
     --conf spark.driver.log.dfsDir=${SPARK_LOG_DIR} \
     --conf spark.driver.log.persistToDfs.enabled=true \
-    --conf spark.driver.memory="${SPARK_DRIVER_MEMORY}" \
+    --conf spark.driver.memory="${SPARK_DRIVER_MEMORY_GB}G" \
     --conf spark.driver.port=${SPARK_DRIVER_PORT} \
     --conf spark.driverEnv.LD_LIBRARY_PATH=${LD_LIBRARY_PATH} \
     --conf spark.eventLog.dir=${SPARK_LOG_DIR} \
@@ -78,8 +75,8 @@ ${SPARK_HOME}/bin/spark-shell \
     --conf spark.executor.cores=${SPARK_EXECUTOR_CORES} \
     --conf spark.executor.extraJavaOptions="-Dio.netty.tryReflectionSetAccessible=true" \
     --conf spark.executor.instances=${SPARK_EXECUTOR_INSTANCES} \
-    --conf spark.executor.memory=${SPARK_EXECUTOR_MEMORY} \
-    --conf spark.executor.memoryOverhead=${SPARK_EXECUTOR_MEMORY_OVERHEAD} \
+    --conf spark.executor.memory="${SPARK_EXECUTOR_MEMORY_GB}G" \
+    --conf spark.executor.memoryOverhead="${SPARK_EXECUTOR_MEMORY_OVERHEAD_GB}G" \
     --conf spark.executorEnv.LD_LIBRARY_PATH=${LD_LIBRARY_PATH} \
     --conf spark.hadoop.javax.jdo.option.ConnectionURL="jdbc:derby:${SPARK_DIRS}/metastore_db;create=true" \
     --conf spark.history.fs.logDirectory=${SPARK_LOG_DIR} \
