f41d21e685524e73bb0670a2eae162401312dc82
Shahar Valiano <shahar.v@neuroblade.ai>
2025-03-11 18:23:54 +0200

    set_spark_env.sh: use camelCase naming for local variables; cosmetics

--- spark/set_spark_env.sh
+++ spark/set_spark_env.sh
@@ -2,25 +2,31 @@
 
 set -eu
 
-# Set Spark options using env. variables.
-# Exported variable are respected by Spark when starting Spark master and worker nodes.
-# Local variables are for setting script options.
-
-# Set Spark master URL and ports
-export SPARK_MASTER_PORT=7078
-export SPARK_DRIVER_PORT=7079
-HOSTNAME=$(hostname)
-MASTER_URL="spark://${HOSTNAME}:${SPARK_MASTER_PORT}"
-export SPARK_LOCAL_HOSTNAME=${HOSTNAME}
+# --------------------------------------------------------------------------------
+# Create Spark folders
+# --------------------------------------------------------------------------------
 
 # Set Spark log and work dirs
 SPARK_LOG_DIR=${SPARK_LOG_DIR:-/tmp/spark-logs}
 mkdir -p $SPARK_LOG_DIR
 
+export SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory=$SPARK_LOG_DIR"
+
+# --------------------------------------------------------------------------------
+# Set Spark options using env. variables.
+# Exported variable are read by Spark when starting the master and worker nodes.
+# --------------------------------------------------------------------------------
+
+# Set Spark master URL and ports
+export SPARK_MASTER_PORT=${SPARK_MASTER_PORT:-7078}
+export SPARK_DRIVER_PORT=${SPARK_DRIVER_PORT:-7079}
+hostname=$(hostname)
+MASTER_URL="spark://${hostname}:${SPARK_MASTER_PORT}"
+export SPARK_LOCAL_HOSTNAME=${hostname}
+
 SPARK_DIRS=${SPARK_DIRS:-/tmp/spark}
 export SPARK_LOCAL_DIRS=${SPARK_DIRS}/temp
 export SPARK_WORKER_DIR=${SPARK_DIRS}/worker
-export SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory=$SPARK_LOG_DIR"
 
 # Set the number of cores for the worker and driver.
 # Make sure the number of worker cores is even, to prevent cache performance issues.
@@ -30,9 +36,9 @@ export SPARK_WORKER_CORES=$(nproc --all)
 # The total max. memory used by the worker includes executors and driver memory
 # (in cluster mode, the driver runs on the worker node), including heap and off-heap
 # memory of each executor.
-TOTAL_MEM_GB=$(free -g -h -t | grep Mem: | sed 's/Gi.*//g' | sed 's/Mem: \+//')
-TOTAL_MEM_GB_ROUNDED=${TOTAL_MEM_GB%.*} # remove the fraction
-export SPARK_WORKER_MEMORY="$TOTAL_MEM_GB_ROUNDED""G"
+totalMemGb=$(free -g -h -t | grep Mem: | sed 's/Gi.*//g' | sed 's/Mem: \+//')
+totalMemGbRounded=${totalMemGb%.*} # remove the fraction
+export SPARK_WORKER_MEMORY="$totalMemGbRounded""G"
 
 # Driver memory of 4G should be enough for most tasks. Spark default is 1G.
 export SPARK_DRIVER_MEMORY=4G
