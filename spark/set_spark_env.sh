1a750d4d41030bd1123cd640dd72e7d1beedb2c5
Shahar Valiano <shahar.v@neuroblade.ai>
2025-03-11 18:41:58 +0200

    set_spark_env.sh: organize and add documentation

--- spark/set_spark_env.sh
+++ spark/set_spark_env.sh
@@ -2,15 +2,16 @@
 
 set -eu
 
-# --------------------------------------------------------------------------------
-# Create Spark folders
-# --------------------------------------------------------------------------------
-
-# Set Spark log and work dirs
-SPARK_LOG_DIR=${SPARK_LOG_DIR:-/tmp/spark-logs}
-mkdir -p $SPARK_LOG_DIR
-
-export SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory=$SPARK_LOG_DIR"
+# This script sets Spark global configuration options, as well as preparing common
+# options to be used by Spark command line tools.
+#
+# There are 3 types of variables used in the script:
+# 1. Spark global options. Those are variables respected by Spark. They are
+#    preceeded by "export".
+# 2. Script parameters which are used to set Spark options, and could be overriden
+#    by the environment. These variables are named in UPPER_CASE style.
+# 3. Local variables which are used for intermediate calculations. These variables
+#    are named in camelCase style.
 
 # --------------------------------------------------------------------------------
 # Set Spark options using env. variables.
@@ -28,9 +29,34 @@ SPARK_DIRS=${SPARK_DIRS:-/tmp/spark}
 export SPARK_LOCAL_DIRS=${SPARK_DIRS}/temp
 export SPARK_WORKER_DIR=${SPARK_DIRS}/worker
 
+# Set Spark log and work dirs
+SPARK_LOG_DIR=${SPARK_LOG_DIR:-/tmp/spark-logs}
+export SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory=$SPARK_LOG_DIR"
+
+# --------------------------------------------------------------------------------
+# Set Spark CPU cores options
+# --------------------------------------------------------------------------------
+
 # Set the number of cores for the worker and driver.
 # Make sure the number of worker cores is even, to prevent cache performance issues.
 export SPARK_WORKER_CORES=$(nproc --all)
+
+# Set the number of cores for each executor and the driver application.
+# Make sure the number of executor cores is even, to prevent cache performance issues.
+# Example calculation: if SPARK_WORKER_CORES is 12, and we set 2 cores for driver and
+# 2 cores for each executor, then we have 5 executors (12 = 5 x 2 + 2).
+SPARK_DRIVER_CORES=${SPARK_DRIVER_CORES:-2}
+SPARK_EXECUTOR_CORES=${SPARK_EXECUTOR_CORES:-2}
+let SPARK_TOTAL_EXECUTOR_CORES=($SPARK_WORKER_CORES-$SPARK_DRIVER_CORES)
+SPARK_EXECUTOR_INSTANCES=${SPARK_EXECUTOR_INSTANCES:-$(bc <<< $SPARK_TOTAL_EXECUTOR_CORES/$SPARK_EXECUTOR_CORES)}
+
+# Set spark.max.cores identical to SPARK_WORKER_CORES, meaning that a single
+# application will be served with all cores.
+SPARK_CORES_MAX=${SPARK_CORES_MAX:-$SPARK_WORKER_CORES}
+
+# --------------------------------------------------------------------------------
+# Set Spark memory options
+# --------------------------------------------------------------------------------
 
 # export SPARK_WORKER_MEMORY for defining the physical memory limit of the worker node.
 # The total max. memory used by the worker includes executors and driver memory
@@ -41,4 +67,9 @@ totalMemGbRounded=${totalMemGb%.*} # remove the fraction
 export SPARK_WORKER_MEMORY="$totalMemGbRounded""G"
 
 # Driver memory of 4G should be enough for most tasks. Spark default is 1G.
-export SPARK_DRIVER_MEMORY=4G
+export SPARK_DRIVER_MEMORY=${SPARK_DRIVER_MEMORY:-4G}
+
+# Set the same amount of memory per executor core.
+SPARK_WORKER_MEMORY_GB=${SPARK_WORKER_MEMORY//G}
+SPARK_EXECUTOR_MEMORY_GB=${SPARK_EXECUTOR_MEMORY_GB:-$(echo "scale=0; x=($SPARK_WORKER_MEMORY_GB-$SPARK_DRIVER_MEMORY_GB)/$SPARK_EXECUTOR_INSTANCES; if(x<1 && x>0) print 0; x" | bc)}
+SPARK_EXECUTOR_MEMORY_OVERHEAD_GB=${SPARK_EXECUTOR_MEMORY_OVERHEAD_GB:-1}
